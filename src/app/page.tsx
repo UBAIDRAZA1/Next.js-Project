608e2f44b3947d3408c3fb96c706c71b6d5b3933
Ubaid Raza <141560709+UBAIDRAZA1@users.noreply.github.com>
2024-11-01 20:24:43 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@
 import Link from 'next/link'
 import React from 'react'
 import { useRouter } from "next/navigation";
-import Card from './components/card/page';
+//import Card from './components/card/page';
 import Image from 'next/image';
-
+ 
 const Home = () => {
   const route = useRouter()
   return (
@@ -14,13 +14,13 @@ const Home = () => {
     <Image src={'/image1.jpeg'}  alt='Image' width={200} height={200} ></Image>
 
     <br/><br/>
-    <Link href='/components/about'><b>Go to About Page</b></Link>
+    <Link href='/components/about'><b>Go to the About Page.</b></Link>
    
-    <div className = 'w-40 h-20 ml-5 mt-10 pl-5 pt-1 rounded-lg text-black   bg-yellow-200' >
+    {/* <div className = 'w-40 h-20 ml-5 mt-10 pl-5 pt-1 rounded-lg text-black   bg-yellow-200' >
     <Card name='Asharib' rollno='2765' day='Tueday' />
     
     <Card name='Ubaid' rollno='2639' day='Wednesday' />
-    </div>
+    </div> */}
    <br /><br />
    
     {/* Button  */}
